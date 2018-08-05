1c89a123abc436cc0dd12212ec60181ab3e244ab
Jack Yang <h36yang@gmail.com>
2018-08-04 23:04:42 -0400

    A bunch of updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faArrowCircleDown, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { PageScrollConfig } from 'ngx-page-scroll';
+import { ParallaxConfig } from 'ngx-parallax';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +12,25 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   customStyle: object;
+  faArrowCircleDown: IconDefinition;
+  parallaxConfig: ParallaxConfig;
+
+  constructor() {
+    PageScrollConfig.defaultScrollOffset = 80;
+    PageScrollConfig.defaultEasingLogic = {
+      ease: (t: number, b: number, c: number, d: number): number => {
+        // easeInOutExpo easing
+        if (t === 0) return b;
+        if (t === d) return b + c;
+        if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+      }
+    };
+  }
 
   ngOnInit() {
+    this.parallaxConfig = { ratio: 0.2 };
+    this.faArrowCircleDown = faArrowCircleDown;
     this.adjustHeight();
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+import { ParallaxModule } from 'ngx-parallax';
+import { InViewportModule, WindowRef } from '@thisissoon/angular-inviewport';
+import { ScrollSpyModule } from '@thisissoon/angular-scrollspy';
 
 import { DataService } from './data.service';
 import { AppComponent } from './app.component';
@@ -11,6 +16,13 @@ import { AboutPuErhComponent } from './about-pu-erh/about-pu-erh.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ProductListComponent } from './product-list/product-list.component';
+
+// Provide window object for browser and a suitable replacement on other platforms
+export const getWindow = () => window;
+export const inViewportProviders: Provider[] = [{
+  provide: WindowRef,
+  useFactory: (getWindow)
+}];
 
 @NgModule({
   declarations: [
@@ -22,9 +34,14 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductListComponent
   ],
   imports: [
+    InViewportModule.forRoot(inViewportProviders),
+    ScrollSpyModule.forRoot(),
     NgbModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgxPageScrollModule,
+    ParallaxModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
