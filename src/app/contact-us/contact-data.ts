ab77da2e763da59a911c3d006af57cdc6d47dbe3
Jack Yang <h36yang@gmail.com>
2018-09-18 00:09:11 -0400

    Fixed more styling

--- src/app/contact-us/contact-data.ts
+++ src/app/contact-us/contact-data.ts
@@ -3,13 +3,13 @@ import { Contact } from "./contact";
 export const CONTACTS: Contact[] = [
     {
         name: "Jasmine Jia",
-        avatar: "../../assets/img/avatars/female-solid.svg",
+        avatar: "../../assets/img/avatars/address-card-solid.svg",
         phone: "647-708-5346",
         email: "jasmine_jia@zctea.ca"
     },
     {
         name: "Jack Yang",
-        avatar: "../../assets/img/avatars/male-solid.svg",
+        avatar: "../../assets/img/avatars/address-card-solid.svg",
         phone: "226-972-8810",
         email: "jack_yang@zctea.ca"
     }
