c58da9709a5c0d1fd0cf1cd7d37eda0f9e990310
Jack Yang <yangja2@otpp.com>
2019-08-14 22:58:23 -0400

    Added new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent }
+  { path: '', component: MainPageComponent },
+  { path: 'product/:id', component: ProductDetailComponent }
 ];
 
 @NgModule({
